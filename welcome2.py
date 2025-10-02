981583b2fd0ef77b202ae6fedc73c2f2d2310740
fming-yu <fming@york.edu>
2025-10-02 11:21:37 -0500

    Add welcome.py program

---
+++ welcome2.py
@@ -0,0 +1,9 @@
+# File: welcome2.py
+# 
+# Sample Python program.
+# Student Name: Professor Ming
+# Class: CSC 323 Modern Technology
+
+name=input("Type your name: ")
+print(f"Hello {name}! Welcome to GitHub Actions!")
+# all done
