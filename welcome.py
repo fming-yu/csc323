57280149c772a9a5b6e97f103da565901a34c8c8
Fuller Ming <fming@york.edu>
2025-10-02 09:21:47 -0500

    Add files via upload

---
+++ welcome.py
@@ -0,0 +1,8 @@
+# File: welcome.py
+# 
+# Sample Python program.
+# Student Name: Professor Ming
+# Class: CSC 323 Modern Technology
+
+name=input("Type your name: ")
+print(f"Hello {name}! Welcome to GitHub Actions!")
